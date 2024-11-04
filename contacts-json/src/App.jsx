474938010bdf691c995db5a3ad09b66727c070cb
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-11-04 11:58:58 -0500

    initial commit for contacts-json

---
+++ contacts-json/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import AddContact from "./components/AddContact";
+import "./index.css";
+import ContactList from "./components/ContactList";
+
+const App = () => {
+
+    const [contacts, setContacts] = useState([
+        { name: "John Doe", id: 1, email: "johndoe@gmail.com" },
+        { name: "Jane Smith", id: 2, email: "janesmith@gmail.com" },
+        { name: "Michael Johnson", id: 3, email: "michaeljohnson@gmail.com" },
+        { name: "Emily Davis", id: 4, email: "emilydavis@gmail.com" },
+        { name: "David Brown", id: 5, email: "davidbrown@gmail.com" }
+    ]);
+
+    const validateContactDetails = (name, email) => {
+        if (name.trim() === "") {
+            alert("Name length must be greater than 0 and should not be just spaces")
+            return false;
+        }
+        if (email.trim() === "") {
+            alert("Email field required and must follow the format of something@something.something")
+            return false;
+        }
+        if (contacts.filter(c => c.name === name.trim()).length) {
+            alert("That contact already exists");
+            return false;
+        }
+
+        return name.trim();
+    }
+
+    const addContact = (e, newName, newEmail) => {
+        e.preventDefault();
+        
+        const parsedName = validateContactDetails(newName, newEmail);
+        if (!parsedName) return;
+
+        const newId = contacts.length + 1;
+        setContacts([
+            ...contacts,
+            { name: parsedName, id: newId, email: newEmail}
+        ]);
+    }
+
+    return (
+        <div className="contacts-app">
+            <AddContact addContact={addContact} />
+            <ContactList contacts={contacts} />
+        </div>
+    );
+};
+
+export default App;
---
+++ contacts-json/src/components/ContactList.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const ContactList = ({contacts}) => {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const searchContacts = (e) => {
+        e.preventDefault();
+        setSearchTerm(e.target.value.toLowerCase());
+    }
+
+    return (
+        <>
+            <h2>Contact List</h2>
+            <form>
+                <input type="text" onChange={searchContacts}/>
+            </form>
+            <table className="contact-table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        contacts.filter(
+                            contact => 
+                                contact.name.toLowerCase().includes(searchTerm) ||
+                                contact.email.toLowerCase().includes(searchTerm) ||
+                                searchTerm.trim() === ""
+                        ).map((contact, index) => {
+                            return (
+                                <tr key={contact.id} className={index % 2 === 0 ? "green-row" : "green-row-dark"}>
+                                    <td>{contact.name}</td>
+                                    <td>{contact.email}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    {
+                        contacts.filter(
+                            contact =>
+                                contact.name.toLowerCase().includes(searchTerm) ||
+                                contact.email.toLowerCase().includes(searchTerm) ||
+                                searchTerm.trim() === ""
+                        ).length === 0 ?
+                            <tr className="green-row">
+                                <td colSpan={2} className="err">
+                                    No results found
+                                </td>
+                            </tr>
+                        : <></>
+                    }
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default ContactList;
