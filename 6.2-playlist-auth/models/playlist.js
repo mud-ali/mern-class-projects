547f8434986282f2abfe1445d376462574efddb6
mudasir <96320211+mud-ali@users.noreply.github.com>
2025-03-14 16:06:03 -0400

    implement model and route changes (25/70)

--- 6.2-playlist-auth/models/playlist.js
+++ 6.2-playlist-auth/models/playlist.js
@@ -5,6 +5,18 @@ const playlistSchema = new mongoose.Schema({
     creator: String,
     numOfSongs: Number,
     likes: Number,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+});
+
+playlistSchema.set("toJSON", {
+    transform: (_, ret) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+    },
 });
 
 const Playlist = mongoose.model("Playlist", playlistSchema);
--- 6.2-playlist-auth/routes/user.js
+++ 6.2-playlist-auth/routes/user.js
@@ -1,11 +1,40 @@
 const bcrypt = require("bcrypt");
+const userRouter = require("express").Router();
 const User = require("../models/user");
 
-playlistRouter.post("/users", (req, res, next) => {
-    try {
-        const { username, password, name } = req.body;
-
-    } catch (error) {
-        next(error);
+userRouter.post("/", async (req, res) => {
+    const { username, name, password } = req.body;
+    if (!password || password.length < 4) {
+        return res
+            .status(400)
+            .json({ error: "Password required. Minimum 4 characters" });
     }
-})
+    const passwordHash = await bcrypt.hash(password, 12);
+    try {
+        const newUser = new User({
+            username,
+            name,
+            passwordHash,
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+userRouter.get("/", async (req, res) => {
+    const { username } = req.body;
+    if (!username) {
+        const users = await User.find({}).populate("playlists");
+        return res.status(200).json(users);
+    }
+
+    const user = await User.findOne({ username }).populate("playlists");
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+    }
+    res.json(user);
+});
+
+module.exports = userRouter;
